0b3fea7dc0002f0f4ee77319662e79a0cfa9189f
Mazalov Alex <mazalovalexey1995@yandex.ru>
2025-05-18 12:13:00 +0300

    Добавллены interface базового набора

---
+++ src/types/index.ts
@@ -0,0 +1,51 @@
+// Товар (Product) — объект
+interface IProduct {
+	id: number;
+	title: string;
+	description: string;
+	image: string;
+	category: string;
+	price: number | null; // null если товар недоступен
+}
+
+// Список товаров (ProductList) — коллекция
+interface IProductList {
+	total: number;
+	items: IProduct[];
+}
+
+// Позиция в корзине (CartItem) — объект
+interface ICartItem {
+	product: IProduct;
+}
+
+// Корзина (Cart) — коллекция
+interface ICart {
+	items: ICartItem[];
+}
+
+// Тип ProductId — это тип поля "id" из интерфейса IProduct.
+type ProductId = IProduct['id'];
+
+// Заказ (Order) — объект
+interface IOrder {
+	items: ProductId[];
+	total: number;
+	address: string;
+	payment: 'Онлайн' | 'При получении';
+	email: string;
+	phone: string;
+}
+
+// Ответ на заказ (OrderResponse) — объект
+interface IOrderResponse {
+	id: number;
+	total: number;
+}
+
+//Ошибки (Errors) — объекты
+enum ErrorType {
+	NoAddress = 'No address',
+	WrongTotal = 'Wrong total',
+	ProductNotFound = 'Product not found'
+}
